74a8b44584011fe6fdecd642b6182a96a1bac5ee
Artur <mrartur0074@gmail.com>
2020-02-11 20:02:16 +0500

    Updates

---
+++ precache-manifest.7b8b85a5ddbb2c773e4ad82e52535071.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146042f31ee15ddf7cfc8137d45919b0",
+    "url": "/my-app-2/index.html"
+  },
+  {
+    "revision": "31391c183edbe936a03f",
+    "url": "/my-app-2/static/css/main.bd140f12.chunk.css"
+  },
+  {
+    "revision": "d32a703bd34f2ace1f7c",
+    "url": "/my-app-2/static/js/2.84afa341.chunk.js"
+  },
+  {
+    "revision": "31391c183edbe936a03f",
+    "url": "/my-app-2/static/js/main.2817b167.chunk.js"
+  },
+  {
+    "revision": "62ecd61207babd1f5517",
+    "url": "/my-app-2/static/js/runtime~main.d3db90ba.js"
+  }
+]);
